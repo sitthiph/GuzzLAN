101008c14a31bf0dbd0923de74eee382bf6f2a4c
Sam Itthiphinyo <47881002+sitthiph@users.noreply.github.com>
2019-04-12 12:29:10 -0400

    Create PlayerList.java

---
+++ GuzzLAN Server/src/PlayerList.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class PlayerList {
+    private List<PlayerNode> playerList;
+
+    public PlayerList(){
+        this.playerList = new LinkedList<PlayerNode>();
+    }
+
+    public
+}
