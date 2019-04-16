cc72ded9ed954218e6ee0c8c7d999b9f108d6b92
asathiye20 <asathiye20@maldenps.org>
2019-04-16 19:43:42 -0400

    Update Player.java

--- GuzzLAN Server/src/PlayerList.java
+++ GuzzLAN Server/src/PlayerList.java
@@ -4,11 +4,31 @@ import java.util.List;
 public class PlayerList {
     private List<PlayerListNode> playerList;
 
-    public PlayerList(){
+    public PlayerList() {
         this.playerList = new LinkedList<PlayerListNode>();
     }
 
-    public void add(PlayerListNode player){
+    public void add(PlayerListNode player) {
+        playerList.add(player);
+    }
 
+    public void add(int index, PlayerListNode player) {
+        playerList.add(index, player);
+    }
+
+    public PlayerListNode remove(int i) {
+        return playerList.remove(i);
+    }
+
+    public PlayerListNode get(int index) {
+        return playerList.get(index);
+    }
+
+    public void clear() {
+        playerList.clear();
+    }
+
+    public boolean isEmpty() {
+        return playerList.isEmpty();
     }
 }
--- GuzzLAN Server/src/PlayerListNode.java
+++ GuzzLAN Server/src/PlayerListNode.java
@@ -1,4 +1,43 @@
 public class PlayerListNode {
+    private Player data;
     private PlayerListNode previous;
     private PlayerListNode next;
+
+    public PlayerListNode() {
+        this(null);
+    }
+
+    public PlayerListNode(Player data) {
+        this(data, null, null);
+    }
+
+    public PlayerListNode(Player data, PlayerListNode previous, PlayerListNode next) {
+        this.data = data;
+        this.previous = previous;
+        this.next = next;
+    }
+
+    public Player getData() {
+        return data;
+    }
+
+    public PlayerListNode getPrevious() {
+        return previous;
+    }
+
+    public PlayerListNode getNext() {
+        return next;
+    }
+
+    public void setData(Player data) {
+        this.data = data;
+    }
+
+    public void setPrevious(PlayerListNode previous) {
+        this.previous = previous
+    }
+
+    public void setNext(PlayerListNode next) {
+        this.next = next;
+    }
 }
