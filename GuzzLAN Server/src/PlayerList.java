0e7beb2f135eb21519ee87423f88dac6765ad076
asathiye20 <asathiye20@maldenps.org>
2019-04-17 12:23:27 -0400

    Update PlayerList.java

--- GuzzLAN Server/src/PlayerList.java
+++ GuzzLAN Server/src/PlayerList.java
@@ -2,13 +2,77 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PlayerList {
-    private List<PlayerListNode> playerList;
+    private PlayerListNode head, tail;
 
-    public PlayerList(){
-        this.playerList = new LinkedList<PlayerListNode>();
+    public PlayerList() {
+        head = null;
+        tail = head;
     }
 
-    public void add(PlayerListNode player){
+    public void add(PlayerListNode player) {
+        if (head == null) {
+            head = player;
+            head.next = head;
+            tail = head;
+            return;
+        }
+    }
 
+    public PlayerListNode remove(int index) {
+        if (index >= size()) {
+            throw new IllegalArgumentException("Index Exceeds Size!!!");
+        }
+
+        PlayerListNode current = head;
+        PlayerListNode previous = null;
+        int i = 0;
+        while (current != tail && i != index) {
+            previous = current;
+            current = current.next;
+            i++;
+        }
+
+        previous = current.next;
+        current.next.
+    }
+
+    public PlayerListNode get(int index) {
+        if (index >= size()) {
+            throw new IllegalArgumentException("Index Exceeds Size!!!");
+        }
+
+        PlayerListNode current = head;
+        int i = 0;
+        while (current != tail && i != index) {
+            prev = prev.next.next;
+            i++;
+        }
+
+        return current;
+    }
+
+    public int size() {
+        if (head == null) {
+            return 0;
+        }
+
+        int count = 1;
+        PlayerListNode current = head;
+
+        while (current != tail) {
+            current = current.next;
+            count++;
+        }
+
+        return count;
+    }
+
+    public void clear() {
+        head = null;
+        tail = head;
+    }
+
+    public boolean isEmpty() {
+        return !((head == null) && (tail == head));
     }
 }
